a48b81ac634e13155acb848f37b84876f04e7410
João Palmeiro <joaommpalmeiro@gmail.com>
2021-12-23 14:18:56 +0000

    Update .svglintrc.figma.js

--- .svglintrc.figma.js
+++ .svglintrc.figma.js
@@ -21,17 +21,35 @@ module.exports = {
         'rule::selector': 'svg'
       },
       {
+        // Source:
+        // - https://github.com/simple-icons/simple-icons/blob/6.3.0/.svglintrc.js#L154
+        // - https://github.com/simple-icons/simple-icons/blob/6.3.0/.svglintrc.js#L857
         // More info:
         // - https://regexr.com/6c4ob
         // - https://regexr.com/6c4ot
-        // - https://github.com/simple-icons/simple-icons/blob/6.3.0/.svglintrc.js#L154
-        // - https://github.com/simple-icons/simple-icons/blob/6.3.0/.svglintrc.js#L857
         // - https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#path_commands
         // d: /^[,a-zA-Z0-9\. -]+$/,
         d: /^[Mm][MmZzLlHhVvCcSsQqTtAaEe0-9-,.\s]+$/,
         fill: true,
         'rule::selector': 'svg > path',
         'rule::whitelist': true
+      }
+    ],
+    custom: [
+      function (reporter, $, ast) {
+        // Source:
+        // - https://github.com/simple-icons/simple-icons/blob/6.3.0/.svglintrc.js#L825
+        // - https://github.com/birjolaxew/svglint/blob/v2.0.0/src/rules/custom.js
+        // - https://github.com/birjolaxew/svglint/blob/v2.0.0/test/custom.spec.js
+        // More info:
+        // - https://cheerio.js.org/
+        // - https://cheerio.js.org/classes/Cheerio.html
+        reporter.name = 'logo-centered';
+
+        const iconPaths = $.find('path').length;
+
+        // reporter.error($.html());
+        reporter.error(iconPaths);
       }
     ]
   }
