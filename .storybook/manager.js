966c2df1e2979740268c69c8d717c2f03a2c9ebb
João Palmeiro <joaommpalmeiro@gmail.com>
2021-12-24 00:12:19 +0000

    Closes #13

---
+++ .storybook/manager.js
@@ -0,0 +1,9 @@
+import { addons } from '@storybook/addons';
+
+// More info:
+// - https://storybook.js.org/docs/react/configure/features-and-behavior
+// - https://storybook.js.org/docs/react/configure/storybook-addons
+// - https://storybook.js.org/docs/react/configure/theming
+addons.setConfig({
+  showPanel: false // Addon panel
+});
--- src/stories/VegaLite.stories.js
+++ src/stories/VegaLite.stories.js
@@ -1,7 +1,10 @@
 import { VegaLite } from '../index';
 
 export default {
-  title: 'Logos/Vega-Lite'
+  title: 'Logos/Vega-Lite',
+  parameters: {
+    layout: 'centered'
+  }
 };
 
-export const Main = () => <VegaLite />;
+export const Main = () => <VegaLite width={240} height={240} />;
